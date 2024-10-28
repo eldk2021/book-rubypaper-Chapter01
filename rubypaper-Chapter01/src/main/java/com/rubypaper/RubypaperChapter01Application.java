d682e0b1ceb2819abf188f4befa4ceb0316c0bfe
eldk2021 <eldk2021@naver.com>
2024-10-29 00:07:33 +0900

    chore: 자바 어플리케이션으로 실행

--- rubypaper-Chapter01/src/main/java/com/rubypaper/RubypaperChapter01Application.java
+++ rubypaper-Chapter01/src/main/java/com/rubypaper/RubypaperChapter01Application.java
@@ -1,13 +1,19 @@
 package com.rubypaper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RubypaperChapter01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RubypaperChapter01Application.class, args);
+		
+		SpringApplication app = new SpringApplication(RubypaperChapter01Application.class);
+		
+		//스프링 MVC를 기반으로 웹 어플리케이션 구동하겠다. 
+		app.setWebApplicationType(WebApplicationType.SERVLET);
+		app.run(args);
 	}
 
 }
