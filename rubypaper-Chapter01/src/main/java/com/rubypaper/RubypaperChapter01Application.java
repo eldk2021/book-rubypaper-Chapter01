f756acc1bf172f2d775b0a91c302c0a883265bc2
eldk2021 <eldk2021@naver.com>
2024-10-28 23:49:18 +0900

    init

---
+++ rubypaper-Chapter01/src/main/java/com/rubypaper/RubypaperChapter01Application.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubypaperChapter01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubypaperChapter01Application.class, args);
+	}
+
+}
