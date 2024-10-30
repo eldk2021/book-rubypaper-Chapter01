c5d54058cc976de484b6843a39460cda5f7ac26e
eldk2021 <eldk2021@naver.com>
2024-10-30 21:51:47 +0900

    test: @AutoConfigureMockMvc 사용하기
    - @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
      @AutoConfigureMockMvc
    
    - 테스트 대상이 아닌 @Service, @Repository가 붙은 객체도 모두 메모리에 올림.

--- rubypaper-Chapter01/src/test/java/com/rubypaper/BoardControllerTest.java
+++ rubypaper-Chapter01/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -7,10 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class BoardControllerTest {
 	
 	@Autowired
@@ -19,10 +23,20 @@ public class BoardControllerTest {
 	@Test
 	public void testHello() throws Exception {
 		
+
+		//get, post, put, delete
+		//isOK, isNotFound, isMethodNotAllowed, isInternalServerError, is(상태코드)
 		mockMvc.perform(get("/hello").param("name", "둘리"))
 		.andExpect(status().isOk())
 		.andExpect(content().string("Hello : 둘리"))
 		.andDo(print());
+		
+		
+		/* MockMvcResultHandlers.print()매서드가 ResultHandler를 구현한 
+		 * ConsolePrintingResultHandler 객체를 리턴.
+		 * -> ConsolePrintingResultHandler를 andDo매서드 인자로 넘겨준다.
+		 * -> 콘솔에 요청/응답과 관련된 정보를 모두 출력.
+		 * */
 		
 	}
 
