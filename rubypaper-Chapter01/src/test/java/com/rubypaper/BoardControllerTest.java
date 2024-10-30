fd5954589dc0ea3e09af6c93fb7f3dc483501755
eldk2021 <eldk2021@naver.com>
2024-10-30 22:00:47 +0900

    test: 내장 톰캣으로 테스트하기
    - @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
      TestRestTemplate 객체를 주입해 사용.

--- rubypaper-Chapter01/src/test/java/com/rubypaper/BoardControllerTest.java
+++ rubypaper-Chapter01/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -1,5 +1,6 @@
 package com.rubypaper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,33 +12,30 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+import com.rubypaper.domain.BoardVO;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BoardControllerTest {
 	
 	@Autowired
-	private MockMvc mockMvc;
+	private TestRestTemplate restTemplate;
 	
 	@Test
 	public void testHello() throws Exception {
 		
-
-		//get, post, put, delete
-		//isOK, isNotFound, isMethodNotAllowed, isInternalServerError, is(상태코드)
-		mockMvc.perform(get("/hello").param("name", "둘리"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("Hello : 둘리"))
-		.andDo(print());
+		// 요청할 url, 응답 결과의 타입 클래스
+		String result = restTemplate.getForObject("/hello?name=둘리", String.class);
+		assertEquals("Hello : 둘리", result);
 		
-		
-		/* MockMvcResultHandlers.print()매서드가 ResultHandler를 구현한 
-		 * ConsolePrintingResultHandler 객체를 리턴.
-		 * -> ConsolePrintingResultHandler를 andDo매서드 인자로 넘겨준다.
-		 * -> 콘솔에 요청/응답과 관련된 정보를 모두 출력.
-		 * */
-		
+	}
+	
+	@Test
+	public void testGetBoard() {
+		BoardVO board = restTemplate.getForObject("/getBoard", BoardVO.class);
+		assertEquals("관리자", board.getWriter());
 	}
 
 }
